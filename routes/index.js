123b54b6067cae5f5b2c4cb431c6491ef87faf10
Jesse Cannon <jessecannon@Jesses-MacBook-Air.local>
2018-03-24 18:26:51 -0400

    Added in the groceries, manager, register and login routes

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,36 @@ router.get('/', function(req, res, next) {
    });
 });
 
+/* GET groceries page. */
+router.get('/groceries', function(req, res, next) {
+  res.render('groceries', { 
+    title: 'Grocery List',
+    message: 'the list of available groceries'
+   });
+});
+
+/* GET manager page. */
+router.get('/manager', function(req, res, next) {
+  res.render('manager', { 
+    title: 'Grocery Manager',
+    message: 'the Grocery Manager'
+   });
+});
+
+// GET: /register
+router.get('/register', (req, res, next) => {
+  res.render('register', {
+    title: 'Register',
+      user: req.user
+  });
+});
+
+// GET: /login
+router.get('/login', (req, res, next) => {
+  res.render('login', {
+    title: 'Login',
+      user: req.user
+  });
+});
+
 module.exports = router;
