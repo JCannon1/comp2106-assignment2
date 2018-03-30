2bb9f5bbb2c37bdf805a1d383dffc0a213335d3f
Jesse Cannon <jessecannon@Jesses-MacBook-Air.local>
2018-03-30 17:46:19 -0400

    Edited the Google Callback URL

--- config/globals.js
+++ config/globals.js
@@ -4,6 +4,6 @@ module.exports = {
         googleClientId: '34987658030-hhdj0st5l8c7rq3cin9s0tb39vcuvq35.apps.googleusercontent.com',
         googleClientSecret: 'v1MvLbpv22IYrrZwRFIMg7Tp',
         //googleCallbackUrl: 'http://localhost:3000/google/callback'
-        googleCallbackUrl: 'http://localhost:3000/google/callback'
+        googleCallbackUrl: 'https://jcannon-comp2106-assignment2.herokuapp.com/google/callback'
     }
 };
