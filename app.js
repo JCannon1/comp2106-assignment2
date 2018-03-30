d7813fffa11a4cccefa102caa1a86dac062ba42c
Jesse Cannon <jessecannon@Jesses-MacBook-Air.local>
2018-03-30 13:48:48 -0400

    Added a bootstrap jumbotron

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
 // references we added
 const mongoose = require('mongoose');
 const config = require('./config/globals');
@@ -21,10 +20,7 @@ var users = require('./routes/users');
 const groceries = require('./routes/groceries');
 const ejsLint = require('ejs-lint');
 
-
 var app = express();
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,12 +34,48 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// db connection
+mongoose.connect(config.db);
+
+// passport configuration
+// app.use(session({
+//   secret: 'any string for salting here',
+//   resave: true,
+//   saveUninitialized: false
+// }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// reference User model
+// const User = require('./models/user');
+
+// passport.use(User.createStrategy());
+
+// google auth strategy
+// passport.use(new googleStrategy({
+//  clientID: config.google.googleClientId,
+//  clientSecret: config.google.googleClientSecret,
+//  callbackURL: config.google.googleCallbackUrl,
+//   profileFields: ['id', 'emails']
+// },
+//   (accessToken, refreshToken, profile, callback) => {
+//       User.findOrCreate({
+//           googleId: profile.id,
+//           username: profile.emails[0].value
+//       }, (err, user) => {
+//           return callback(err, user);
+//       });
+//   }
+// ));
+
+// session management for users
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/groceries', groceries);
-
-// db connection
-mongoose.connect(config.db);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
